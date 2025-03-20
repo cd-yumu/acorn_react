9c359173401c3d5679af93e599c7705596772834
EugeneChoi <yoojin7526@naver.com>
2025-03-20 17:42:51 +0900

    Step10 Gemini AI API 를 이용해 코딩 문제 체점하기 추가

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,19 +1,101 @@
 // src/pages/Home.jsx
 
 import axios from 'axios';
-import React from 'react';
+import React, { useRef, useState } from 'react';
+import { Alert, Button, Form } from 'react-bootstrap';
+import Markdown from 'react-markdown';
 
 function Home(props) {
+    
+
     return (
         <div>
-            <h1>Index Page.</h1>
+            <h1>인덱스 페이지 입니다.</h1>
             <button onClick={()=>{
                 axios.get("/ping")
-                .then(res => alert(res.data))
-                .catch(err => alert("응답하지 않음"));
-            }}>Ping 요청</button>
+                .then(res=>{
+                    alert(res.data);
+                })
+                .catch(error=>{
+                    alert("응답하지 않음");
+                });
+            }}>Ping 요청 해보기</button>
+
+            <Quiz />
+
         </div>
     );
+}
+
+function Quiz(){
+    let quiz = "콘솔창에 1~10 까지 순서대로 출력하는 code를 javascript 로 작성해 보세요";
+    
+    const inputAnswer = useRef();
+
+    const handleSubmit = ()=>{
+
+        const myAnswered = inputAnswer.current.value;
+
+        //질문과 입력한 답을 json 으로 전송한다.
+        axios.post("/gemini/quiz", {
+            quiz,
+            answer:myAnswered
+        })
+        .then(res=>{
+            // res.data 는 이런 모양의 object 이다. {isCorrect: true or false, comment:"markdown"}
+            console.log(res.data);
+            setState({
+                ...state,
+                ...res.data,
+                isAnswered:true,
+                myAnswered:myAnswered
+            });
+        })
+        .catch(error=>console.log(error));
+    }
+
+    const [state, setState] = useState({
+        isAnswered:false,
+        myAnswered:""
+    });
+
+
+    return (
+        <>
+            { state.isAnswered?
+            <div>
+                <Form.Group className='mb-3'>
+                    <Form.Label>{quiz}</Form.Label>
+                    <Form.Control defaultValue={state.myAnswered} as="textarea" rows="10"></Form.Control>
+                </Form.Group>
+                
+
+                <h3>체점 결과</h3>
+                {/* <p>{JSON.stringify(state.isCorrect)}</p> */}
+                { state.isCorrect?
+                    <Alert variant='success'>정답 입니다</Alert>
+                    :
+                    <Alert variant='danger'>오답 입니다</Alert>
+                }
+                <div><Markdown>{state.comment}</Markdown></div>
+
+                <Button variant='warning' className='me-3'>&larr;다시 풀기</Button>
+                <Button variant='success'>다음 문제&rarr;</Button>
+            </div>
+            :
+            <div>
+                <Form.Group className='mb-3'>
+                    <Form.Label>{quiz}</Form.Label>
+                    <Form.Control ref={inputAnswer} as="textarea" rows="10"></Form.Control>
+                </Form.Group>
+                <Button onClick={handleSubmit}>제출</Button>
+            </div>
+            }
+
+
+
+        </>
+    )
 }
 
 export default Home;
--- src_try/components/BsNavBar.jsx
+++ src_try/components/BsNavBar.jsx
@@ -1,8 +1,17 @@
 import React from 'react';
-import { Container, Nav, Navbar } from 'react-bootstrap';
+import { Button, Container, Nav, Navbar } from 'react-bootstrap';
+import { useDispatch, useSelector } from 'react-redux';
 import { NavLink } from 'react-router-dom';
 
 function BsNavBar(props) {
+
+    // 로그인 사용자 정보 (redux store 에서 관리됨)
+    const userInfo = useSelector(state=>state.userInfo);
+
+    // redux store 의 값을 바꿔줄 dispath 생성
+    const dispatch = useDispatch();
+
+
 
     return (
         <>
@@ -11,13 +20,39 @@ function BsNavBar(props) {
                     <Navbar.Brand as={NavLink} className='text-white'>Try</Navbar.Brand>
                     <Navbar.Toggle aria-controls="one"/>
                     <Navbar.Collapse id="one">
-                        {/*  */}
+                        {/* 기본 네비바 구성 */}
                         <Nav className='me-auto'>
                             <Nav.Link as={NavLink} to="/" className='text-white'>Home</Nav.Link>
                             <Nav.Link as={NavLink} to="/posts" className='text-white'>Post</Nav.Link>
                         </Nav>
 
-                        {/*  */}
+                        {/* 로그인 관련 */}
+                        {
+                            // userInfo 상태값을 사용해 로그인 사용자의 정보를 저장한다.
+                            userInfo ?
+                                // 사용자 정보가 있으면 - 정보보기, 로그아웃 제공 
+                                <Nav>
+                                    
+                                </Nav>
+                            :
+                                // 사용자 정보가 없으면 - 로그인, 회원가입 제공
+                                <Nav>
+                                    <Button onClick={()=>{
+                                        // 로그인 모달창 띄우기
+                                        const action = {
+                                            type : "LOGIN_MODAL", 
+                                            payload : {
+                                                title : "Login Form.",
+                                                show : "true"
+                                            }
+                                        };
+                                        dispatch(action);       // store 에서 관리되는 로그인 관련 모달창 데이터의 show 값이 true 가 되면서 모달창이 보여진다.
+                                        // 모달창은 App.jsx 에 컴포넌트로 구성되어 있는데 평소 false 일 때는 안 보이다가
+                                        // 방금 true 값으로 변경한 것에 의해 보이게 된다.
+                                    }}>Login</Button>
+                                    <Button>Sign-in</Button>
+                                </Nav>
+                        }
 
                     </Navbar.Collapse>
                 </Container>
